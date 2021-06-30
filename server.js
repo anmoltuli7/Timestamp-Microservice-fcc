f5988cdd91b840d46a1f1830be9ab2d2a4c75173
anmoltuli7 <you@example.com>
2021-06-30 12:23:56 +0530

    timestamp 1.0.0

--- server.js
+++ server.js
@@ -20,13 +20,20 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:timestamp", function (req, res) {
+  let timestamp = req.params.timestamp;
+
+  if (!timestamp.match(/-/g)){
+    timestamp = +timestamp;
+  }
+
+  let date = new Date(timestamp);
+  res.json({unix: date.valueOf(), utc: date.toUTCString() });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
